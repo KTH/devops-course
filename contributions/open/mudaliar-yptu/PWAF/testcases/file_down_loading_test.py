3f7bccc695654f8c0b8eb4fbe067961af9416dd0
mudaliarkartik <mudaliarkartik@yahoo.com>
2019-04-30 12:16:46 +0200

    Fix bug at File download

--- contributions/open/mudaliar-yptu/PWAF/testcases/file_down_loading_test.py
+++ contributions/open/mudaliar-yptu/PWAF/testcases/file_down_loading_test.py
@@ -5,25 +5,14 @@
 """
 from pages.file_down_loading_page import FileDownLoadingPage
 from pages.welcome_page import WelcomePage
-from utility.drivermanager import DriverManagerFirefox, DriverManagerChrome
-from nose.plugins.attrib import attr
+from utility.drivermanager import DriverManager
 
-@attr(group=['kth'])
-class FileDownLoadingTestFirefox(DriverManagerFirefox):
+
+class FileDownLoadingTest(DriverManager):
     def test_file_downloading_functionality(self):
         welcome_page = WelcomePage(self.driver)
         welcome_page.verify_welcome_page().click_on_link("File Download")
 
         file_download = FileDownLoadingPage(self.driver)
         file_download.verify_file_downloader_page()
-        file_download.verify_file_downloading("shank.txt")
-
-@attr(group=['kth'])
-class FileDownLoadingTestChrome(DriverManagerChrome):
-    def test_file_downloading_functionality(self):
-        welcome_page = WelcomePage(self.driver)
-        welcome_page.verify_welcome_page().click_on_link("File Download")
-
-        file_download = FileDownLoadingPage(self.driver)
-        file_download.verify_file_downloader_page()
-        file_download.verify_file_downloading("shank.txt")
+        file_download.verify_file_downloading("Culture.ppt")
