59455ffc73505234ea8249ec02f4545890e43d47
Ackuq@ <axel@pettersson.cc>
2021-03-24 15:10:59 +0100

    Add type definition for JS

--- contributions/course-automation/axp-chrigu/dashboard-generator/src/index.js
+++ contributions/course-automation/axp-chrigu/dashboard-generator/src/index.js
@@ -1,5 +1,26 @@
 const core = require("@actions/core");
 
+/**
+ * Result type of data collection
+ * @typedef Result
+ * @type {{
+ *  summary: {
+ *    nrTotal: number
+ *  },
+ *  [year: number]: {
+ *      nrTotal: number,
+ *      categories: {
+ *          name: string,
+ *          tasks: {
+ *              authors: string[],
+ *              link: string,
+ *              title: string
+ *          }[]
+ *       }[]
+ *  }
+ * }}
+ */
+
 try {
     const time = new Date().toTimeString();
     core.setOutput("dashboard", `dashboard - ${time}`);
