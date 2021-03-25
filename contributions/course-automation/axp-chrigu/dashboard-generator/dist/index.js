41b2580ff7e7e14e731e2aa675ca5755f701ec7c
Ackuq@ <axel@pettersson.cc>
2021-03-25 16:02:54 +0100

    Build new action

--- contributions/course-automation/axp-chrigu/dashboard-generator/dist/index.js
+++ contributions/course-automation/axp-chrigu/dashboard-generator/dist/index.js
@@ -395,10 +395,157 @@ exports.toCommandValue = toCommandValue;
 
 /***/ }),
 
+/***/ 14:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const fs = __nccwpck_require__(747);
+const path = __nccwpck_require__(622);
+const { title } = __nccwpck_require__(765);
+
+// collectDashboardData().then(data => {
+//     console.log(data);
+// });
+
+const repoUrl = "https://github.com/KTH/devops-course/tree/2021/";
+
+// Traverse the project directory and look for all the contributions folders, collect task data on every folder
+async function* traverse(dir) {
+
+    for await (const d of await fs.promises.opendir(dir)) {
+        const a = new Map();
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) {
+            if(d.name.includes('contributions')){
+                const currentTime = new Date();
+                // If folder name contains the year, use it, otherwise it is contributions for the current year
+                year = isNaN(d.name.slice(-4)) ? currentTime.getFullYear() : parseInt(d.name.slice(-4))
+                // Collect the data of a given years contributions
+                yield* a.set(year, collectYearData(entry));
+            }
+            else{
+                // Found non-task associated file, skip
+                yield* traverse(entry);
+            }
+        }
+    }
+}
+
+async function* collectYearData(dir){
+    const categories = [];
+    let taskCount = 0;
+    // Go trough the directory and find all category directories
+    for await (const categoryDir of await fs.promises.opendir(dir)) {
+        if(categoryDir.isDirectory()){
+            let categoryData = [];
+            let entry = "";
+            const categoryPath = path.join(dir, categoryDir.name);
+            // Go trough all the task in a given category directory
+            for await (const taskDir of await fs.promises.opendir(categoryPath)) {
+                if(taskDir.isDirectory()){
+                    // If folder is called weekX, its the presentation folder where tasks are divided into weeks,
+                    // iterate over the specific week folders
+                    if(taskDir.name.includes("week") || taskDir.name.includes("weak")){
+                        entry = path.join(categoryPath, taskDir.name); 
+                        for await (const presentationTaskDir of await fs.promises.opendir(entry)) {
+                            if(presentationTaskDir.isDirectory()){
+                                const presentationEntry = path.join(entry, presentationTaskDir.name+"/README.md");
+                                
+                                fs.readFile(presentationEntry, 'utf-8', (err, data) => {
+                                    if(err){
+                                        //console.error(err);
+                                    }
+                                    else{
+                                        // Parse md file for authors, title and link
+                                        const {title: t, authors: a} = parseMd(data);
+                                        taskCount++;
+                                        categoryData.push({'title': t, 'authors': a, 'link': repoUrl+entry});
+                                    }
+                                })
+                            }
+                        }
+                    }
+                    // Not the presentation folder, task folders are located here, read the task README and collect the data
+                    else{
+                        entry = path.join(categoryPath, taskDir.name+"/README.md");
+                        fs.readFile(entry, 'utf-8', (err, data) => {
+                            if(err){
+                                //console.error(err);
+                            }
+                            else{
+                                // Parse md file for authors, title and link
+                                const {title: t, authors: a} = parseMd(data);
+                                taskCount++;
+                                categoryData.push({'title': t, 'authors': a, 'link': repoUrl+entry});
+                            }
+                        })
+                    }
+                }
+            }
+            categories.push({'name': categoryDir.name, 'tasks': categoryData});
+        }
+    }
+    categories.unshift(taskCount);
+    yield* categories;
+}
+
+// Parse a given string in md format, return authors, title and link of the task
+function parseMd(mdContent){
+    
+    //Find title
+    const titleRegex = /^# (.*$)/gim
+    let title = titleRegex.exec(mdContent);
+    if(title != null) {
+        title = title[1];
+    }
+
+    // Find author
+    const authorRegex = /(.*@kth.se.*)/gim
+    let authors = mdContent.match(authorRegex);
+    if(authors != null ) authors = authors.map(authString => {
+        // Remove any unnessecary characters from beginning of author string
+        let i = 0;
+        while(i < authString.length) {
+            if(authString[i] == " " | authString[i] == "*" | authString[i] == "-"){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return authString.substring(i);
+    });
+
+    return {'title': title, 'authors': authors};
+}
+
+async function collectDashboardData() {
+    const year = new Map();
+    let totalTaskCount = 0;
+    // Wait for all data items to be collected
+    for await (const p of traverse('./')){
+        const allItems = [];
+        for await (const item of p[1]) {
+            allItems.push(item);
+        }
+        // add number of tasks in year to the total number of tasks
+        totalTaskCount += allItems[0];
+        // Map the year of the contributions to its number of tasks and the tasks
+        year.set(p[0], {'nrTotal': allItems[0], 'categories': allItems.slice(1)});
+    }
+    const returnData = {'summary':{ 'nrTotal': totalTaskCount}, year}
+    return returnData;    
+}
+
+module.exports.collectDashboardData = collectDashboardData;
+
+/***/ }),
+
 /***/ 736:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(36);
+const { collectDashboardData } = __nccwpck_require__(14);
+const { parseJson } = __nccwpck_require__(589);
 
 /**
  * Result type of data collection
@@ -421,12 +568,107 @@ const core = __nccwpck_require__(36);
  * }}
  */
 
-try {
-    const time = new Date().toTimeString();
-    core.setOutput("dashboard", `dashboard - ${time}`);
-} catch (error) {
-    core.setFailed(error.message);
+// try {
+//     const time = new Date().toTimeString();
+//     core.setOutput("dashboard", `dashboard - ${time}`);
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
+
+async function generateDashboard(){
+    try{
+        const data = await collectDashboardData();
+        const md = parseJson(data);
+        core.setOutput("dashboard", md);
+    } catch(error) {
+        core.setFailed(error.message);
+    }
 }
+
+generateDashboard();
+
+/***/ }),
+
+/***/ 589:
+/***/ ((module) => {
+
+/**
+ * Result type of data collection
+ * @typedef Result
+ * @type {{
+ *  summary: {
+ *    nrTotal: number
+ *  },
+ *  year: {
+*  [year: number]: {
+*      nrTotal: number,
+*      categories: {
+*          name: string,
+*          tasks: {
+*              authors: string[],
+*              link: string,
+*              title: string
+*          }[]
+*       }[]
+*  }
+ * }
+ * }}
+ */
+
+/**
+ *
+ * @param {Result[number]["categories"][number]["tasks"][number]["authors"]} authors
+ * @returns
+ */
+const parseAuthors = (authors) => {
+    if(authors) return authors.join(" & ");
+    return "Authors not found";
+};
+
+/**
+ *
+ * @param {string[]} markdown
+ * @param {Result[number]["categories"][number]} category
+ */
+const parseCategory = (markdown, category) => {
+    markdown.push(`### ${category.name}`);
+    category.tasks.forEach((task) => {
+        markdown.push(
+            `- ${parseAuthors(task.authors)} - [${task.title || "Title not found"}](${task.link})`
+        );
+    });
+    return markdown;
+};
+
+/**
+ * Parse the JSON object to a markdown string
+ * @param {Result} result
+ * @returns {string}
+ */
+const parseJson = (result) => {
+    let markdown = [];
+    // Add title
+    markdown.push("# Dashboard");
+    // Add total summary
+    markdown.push(`Total submissions: ${result.summary.nrTotal}`);
+
+    // Add years
+    [...result.year.keys()]
+        .sort((a, b) => parseInt(b) - parseInt(a))
+        .forEach((yearKey) => {
+            const year = result.year.get(yearKey);
+            markdown.push(`## ${yearKey}`);
+            markdown.push(`Submissions this year: ${year.nrTotal}`);
+
+            year.categories.forEach((category) => {
+                markdown = parseCategory(markdown, category);
+            });
+        });
+
+    return markdown.join("\r\n");
+};
+
+module.exports.parseJson = parseJson;
 
 
 /***/ }),
@@ -452,6 +694,14 @@ module.exports = require("os");;
 
 "use strict";
 module.exports = require("path");;
+
+/***/ }),
+
+/***/ 765:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("process");;
 
 /***/ })
 
