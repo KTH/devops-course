0c9d934502ae44c44cde9d59eb922fb251b20f95
Kalle Pettersson <kalpet@kth.se>
2021-03-25 13:35:33 +0100

    test: testing how to fail a push (3)

--- contributions/course-automation/kalpet-andnil5/action.js
+++ contributions/course-automation/kalpet-andnil5/action.js
@@ -11,8 +11,9 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  return 1
+  // console.log(`The event payload: ${payload}`);
+  core.setFailed();
+
 } catch (error) {
   core.setFailed(error.message);
 }
