029b48750feb7dc7978b90b6938e35892d351148
Christopher Gustafson <chrigu@kth.se>
2021-03-26 15:20:54 +0100

    Added number of submissions summary table

--- contributions/course-automation/axp-chrigu/dashboard-generator/src/markdown.js
+++ contributions/course-automation/axp-chrigu/dashboard-generator/src/markdown.js
@@ -47,6 +47,68 @@ const parseCategory = (markdown, category) => {
 };
 
 /**
+ * Generate a map with number of submissions each year per category
+ * @param {Map} yearsData
+ * @returns {Map}
+ */
+const getYearPerCategory = (yearsData) => {
+    const yearPerCategory = new Map();
+    for(const [key, value] of yearsData.entries()){
+        value.categories.forEach(category => {
+            const categoryData = yearPerCategory.get(category.name);
+            if(categoryData){
+                categoryData[key] = category.tasks.length;
+            }
+            else{
+                const c = {};
+                c[key] = category.tasks.length;
+                yearPerCategory.set(category.name, c);
+            }
+        })
+    };
+    
+    return yearPerCategory;
+}
+
+/**
+ * Generate a markdown table that summarises number of sumbissions per year and category
+ * @param {Map} yearsData
+ * @returns {string}
+ */
+const generateSummaryTable = (yearsData) => {
+    const data = getYearPerCategory(yearsData);
+    let markdown = [];
+    const years = Array.from(yearsData.keys());
+    // Header
+    let header = "| Category |";
+    years.forEach(year => {
+        header += ` ${year} |`;
+    });
+    markdown.push(header);
+    
+    // Specify alignment
+    markdown.push("|-|-|-|")
+
+    // Generate rows in the form | category | submissions year X | submissions year X+1 | ... |
+    for(const [key, value] of data){
+        let row = `| ${key} |`;
+        years.forEach(year => {
+            const count = value[year] ? value[year] : 0;
+            row += ` ${count} |`;
+        })
+        markdown.push(row);
+    }
+
+    let totalRow = `| **Total** |`;
+    years.forEach(year => {
+        totalRow += ` ${yearsData.get(year).nrTotal} |`;
+    })
+    markdown.push(totalRow);
+    
+    return markdown.join("\r\n");
+}
+
+/**
  * Parse the JSON object to a markdown string
  * @param {Result} result
  * @returns {string}
@@ -57,6 +119,9 @@ const parseJson = (result) => {
     markdown.push("# Dashboard");
     // Add total summary
     markdown.push(`Total submissions: ${result.summary.nrTotal}`);
+    // Add table summary
+    markdown.push("## Submissions per year");
+    markdown.push(generateSummaryTable(result.year));
 
     // Add years
     [...result.year.keys()]
