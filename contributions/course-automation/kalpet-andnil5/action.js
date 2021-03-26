8fea18fe2292d4c1076e751e8898db661b1dc5ad
Kalle Pettersson <kalpet@kth.se>
2021-03-26 11:10:35 +0100

    test: testing pullreq 2

--- contributions/course-automation/kalpet-andnil5/action.js
+++ contributions/course-automation/kalpet-andnil5/action.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-import {context, getOctokit } from '@actions/github';
+const github = require('@actions/github');
 const fs = require('fs');
 const { join, resolve } = require('path');
 // const { parseKTHEmail, readFile } = require('./parser');
@@ -11,33 +11,30 @@ const parser = {
     // TODO: FIXA FELHANTERING
     const data = this.readFile(file);
     const ma = data.match(/-----[^-----]+-----/)[0];
-    const res = ma.match(/(([\w\d\._%+-]+)@kth.se)/g)
-      .map(mail => mail.replace('@kth.se', ''));
+    const res = ma.match(/(([\w\d\._%+-]+)@kth.se)/g);
     return res;
   },
 };
 
-const root = join(resolve(__dirname), '..', '..', '..');
+const root = join(resolve(__dirname));
 
-const kthIDs = [
-  'andnil5',
-  'johanbes',
-  'KallePettersson'
-];
+// Felhantering...
+const kthIDs = parser.readFile(join(root, 'kth-ids.txt')).split(/\n/);
+console.log('KTH id', kthIDs);
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
 
   // const client = new GitHub(core.getInput('token', {required: true}))
-  const client = getOctokit(core.getInput('token'));
+  const client = github.getOctokit(core.getInput('token'));
 
-  const base = context.payload.pull_request.base.sha;
-  const head = context.payload.pull_request.head.sha;
+  const base = github.context.payload.pull_request.base.sha;
+  const head = github.context.payload.pull_request.head.sha;
 
   if (!base || !head) {
     core.setFailed(
-      `The base and head commits are missing from the payload for this ${context.eventName} event. ` +
+      `The base and head commits are missing from the payload for this ${github.context.eventName} event. ` +
         "Please submit an issue on this action's GitHub repo."
     );
   }
@@ -46,8 +43,8 @@ try {
   client.repos.compareCommits({
     base,
     head,
-    owner: context.repo.owner,
-    repo: context.repo.repo
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
   }).then(response => {
     if (response.status !== 200) throw Error('Could not fetch changed files!');
     const files = response.data.files;
@@ -57,12 +54,15 @@ try {
       .map(file => file.filename.split('/'))
       .filter(file => file.length > 3 && file[0] === 'contributions' );
     if (filteredFiles.length < 1) throw Error('Could not find path to README.md');
-    const readme = [...filteredFiles[0].splice(0,3), 'README.md'].join('/');
+    const readme = join([...filteredFiles[0].splice(0,3), 'README.md']);
     console.log('File', readme);
     const ids = parser.parseKTHEmail(readme);
     console.log('In parse...', ids);
     const correctIDs = ids.filter(id => kthIDs.includes(id));
     console.log(correctIDs, ids);
+    if(correctIDs.length === 0){
+      core.setFailed("Invalid KTHids in README file");
+    }
 
   }).catch(error => {
     core.setFailed(error.message);
--- contributions/course-automation/kalpet-andnil5/dist/index.js
+++ contributions/course-automation/kalpet-andnil5/dist/index.js
@@ -3,14 +3,10 @@ module.exports =
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 957:
-/***/ ((__unused_webpack_module, __webpack_exports__, __nccwpck_require__) => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(608);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_0__);
 const core = __nccwpck_require__(66);
-
+const github = __nccwpck_require__(608);
 const fs = __nccwpck_require__(747);
 const { join, resolve } = __nccwpck_require__(622);
 // const { parseKTHEmail, readFile } = require('./parser');
@@ -22,33 +18,30 @@ const parser = {
     // TODO: FIXA FELHANTERING
     const data = this.readFile(file);
     const ma = data.match(/-----[^-----]+-----/)[0];
-    const res = ma.match(/(([\w\d\._%+-]+)@kth.se)/g)
-      .map(mail => mail.replace('@kth.se', ''));
+    const res = ma.match(/(([\w\d\._%+-]+)@kth.se)/g);
     return res;
   },
 };
 
-const root = join(resolve(__dirname), '..', '..', '..');
+const root = join(resolve(__dirname));
 
-const kthIDs = [
-  'andnil5',
-  'johanbes',
-  'KallePettersson'
-];
+// Felhantering...
+const kthIDs = parser.readFile(__nccwpck_require__.ab + "kth-ids.txt").split(/\n/);
+console.log('KTH id', kthIDs);
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_0__.context, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
 
   // const client = new GitHub(core.getInput('token', {required: true}))
-  const client = (0,_actions_github__WEBPACK_IMPORTED_MODULE_0__.getOctokit)(core.getInput('token'));
+  const client = github.getOctokit(core.getInput('token'));
 
-  const base = _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request.base.sha;
-  const head = _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload.pull_request.head.sha;
+  const base = github.context.payload.pull_request.base.sha;
+  const head = github.context.payload.pull_request.head.sha;
 
   if (!base || !head) {
     core.setFailed(
-      `The base and head commits are missing from the payload for this ${_actions_github__WEBPACK_IMPORTED_MODULE_0__.context.eventName} event. ` +
+      `The base and head commits are missing from the payload for this ${github.context.eventName} event. ` +
         "Please submit an issue on this action's GitHub repo."
     );
   }
@@ -57,8 +50,8 @@ try {
   client.repos.compareCommits({
     base,
     head,
-    owner: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo.owner,
-    repo: _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.repo.repo
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
   }).then(response => {
     if (response.status !== 200) throw Error('Could not fetch changed files!');
     const files = response.data.files;
@@ -68,12 +61,15 @@ try {
       .map(file => file.filename.split('/'))
       .filter(file => file.length > 3 && file[0] === 'contributions' );
     if (filteredFiles.length < 1) throw Error('Could not find path to README.md');
-    const readme = [...filteredFiles[0].splice(0,3), 'README.md'].join('/');
+    const readme = join([...filteredFiles[0].splice(0,3), 'README.md']);
     console.log('File', readme);
     const ids = parser.parseKTHEmail(readme);
     console.log('In parse...', ids);
     const correctIDs = ids.filter(id => kthIDs.includes(id));
     console.log(correctIDs, ids);
+    if(correctIDs.length === 0){
+      core.setFailed("Invalid KTHids in README file");
+    }
 
   }).catch(error => {
     core.setFailed(error.message);
@@ -6062,46 +6058,6 @@ module.exports = require("zlib");;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	__nccwpck_require__.ab = __dirname + "/";/************************************************************************/
