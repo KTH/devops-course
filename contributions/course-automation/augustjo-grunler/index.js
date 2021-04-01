363c9938fe0904ec523150132537dab41ceda00f
Daniel <grunler@kth.se>
2021-04-01 16:42:00 +0200

    update action, add token

--- contributions/course-automation/augustjo-grunler/index.js
+++ contributions/course-automation/augustjo-grunler/index.js
@@ -1,15 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+async function doSomething() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const token = core.getInput('repo-token');
+    const octokit = github.getOctokit(token);
+
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  } 
 }
+
+doSomething()
+
