835d32d9ee4bb086bfcbe527f50d2ee764e8f603
August Jönsson <Augustjo@kth.se>
2021-04-05 11:11:46 +0200

    update index.js

--- contributions/course-automation/augustjo-grunler/index.js
+++ contributions/course-automation/augustjo-grunler/index.js
@@ -45,7 +45,7 @@ async function getChangedFiles(octokit, owner, repo, dir, callingBranch='master'
     repo: repo,
     dir: dir
   }).then(data =>{ 
-    console.log(data)
+    console.log(data.content)
     x = atob(data.content)
     console.log(x)
   }).catch(err => {
