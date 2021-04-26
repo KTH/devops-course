9706c782fd2a8c93df57c9582cda0400063ffd1c
Hilaire Bouaddi <hilaire.bouaddi@gmail.com>
2021-04-26 15:49:29 +0200

    update

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -33,7 +33,7 @@ print(feedbacked)
 
 #THIS SECTION CREATES LABELS IF THEY DON'T EXIST IN THE REPO
 label_names = ["feedbackable", "feedback claimed", "feedbacked"]
-colors = ["yellow", "red", "black"]
+colors = ["FFDC73", "97B1F3", "3D5BA5"]
 for i in range(3):
     repo.create_label(label_names[i], colors[i])
 
